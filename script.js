2db868c949a2af3223051640622d43fb4aa92a37
Bandari <ambandarim@gmail.com>
2026-08-13 06:51:36 +0330

    feat: add click event listeners for buttons

--- script.js
+++ script.js
@@ -9,3 +9,11 @@ console.log(getComputerChoice());
 console.log(getComputerChoice());
 console.log(getComputerChoice());
 console.log(getComputerChoice());
+
+function playRound(choice) {
+    console.log('player picked', choice)
+}
+
+document.getElementById('rock').addEventListener('click', () => playRound('rock'));
+document.getElementById('paper').addEventListener('click', () => playRound('paper'));
+document.getElementById('scissors').addEventListener('click', () => playRound('scissors'));
