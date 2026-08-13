c83c58b9bf422b51c54a99e9afe4cdd7d6d8955f
Bandari <ambandarim@gmail.com>
2026-08-13 06:46:52 +0330

    feat: add getComputerChoice function

---
+++ script.js
@@ -0,0 +1,11 @@
+function getComputerChoice() {
+    const choices = ['rock', 'paper', 'scissors'];
+    const randomIndex = Math.floor(Math.random() * 3);
+    return choices[randomIndex];
+}
+
+// Test
+console.log(getComputerChoice());
+console.log(getComputerChoice());
+console.log(getComputerChoice());
+console.log(getComputerChoice());
